5a1c945a7bf2cedf1721295abd7ab9ebd2bc6ed2
mijsu <mijsuofficial@gmail.com>
2025-11-30 18:51:39 +0800

    Update firebase.ts

--- server/firebase.ts
+++ server/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAtXt_mx217dS4KmIzQ78_KafufDwuGhm4",
-  authDomain: "careerquest-7a741.firebaseapp.com",
-  projectId: "careerquest-7a741",
-  storageBucket: "careerquest-7a741.firebasestorage.app",
-  messagingSenderId: "864822379969",
-  appId: "1:864822379969:web:2e86263d38376c9a219b40"
+  apiKey: "",
+  authDomain: "m",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
